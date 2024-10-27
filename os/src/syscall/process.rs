b0cc9ca502ea0f812180c83bcce8c8427bf7dc25
yonchicy <1079279184@qq.com>
2024-10-27 11:31:40 +0800

    merge ch4

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -9,12 +9,16 @@ use crate::{
         add_task, current_task, current_user_token, exit_current_and_run_next,
         suspend_current_and_run_next, TaskStatus,
     },
+    timer::get_time_us,
 };
 
 #[repr(C)]
 #[derive(Debug)]
+/// Time
 pub struct TimeVal {
+    /// sec
     pub sec: usize,
+    /// usec
     pub usec: usize,
 }
 
@@ -22,11 +26,11 @@ pub struct TimeVal {
 #[allow(dead_code)]
 pub struct TaskInfo {
     /// Task status in it's life cycle
-    status: TaskStatus,
+    pub status: TaskStatus,
     /// The numbers of syscall called by task
-    syscall_times: [u32; MAX_SYSCALL_NUM],
+    pub syscall_times: [u32; MAX_SYSCALL_NUM],
     /// Total running time of task
-    time: usize,
+    pub time: usize,
 }
 
 /// task exits and submit an exit code
@@ -43,12 +47,25 @@ pub fn sys_yield() -> isize {
     0
 }
 
+/// get sys pid
 pub fn sys_getpid() -> isize {
     trace!("kernel: sys_getpid pid:{}", current_task().unwrap().pid.0);
     current_task().unwrap().pid.0 as isize
 }
 
+/// Forks a new process by duplicating the current task.
+///
+/// # Returns
+///
+/// - `isize`: The PID of the new process in the parent process's context, or 0 in the child process's context.
+///
+/// # Description
+///
+/// This function creates a new process by cloning the current process. After a successful fork,
+/// the parent process and the child process will continue executing separately. The parent process
+/// receives the PID of the child process, while the child process receives 0 as its return value.
 pub fn sys_fork() -> isize {
+    // Log the fork operation information
     trace!("kernel:pid[{}] sys_fork", current_task().unwrap().pid.0);
     let current_task = current_task().unwrap();
     let new_task = current_task.fork();
@@ -63,6 +80,7 @@ pub fn sys_fork() -> isize {
     new_pid as isize
 }
 
+/// sys exec
 pub fn sys_exec(path: *const u8) -> isize {
     trace!("kernel:pid[{}] sys_exec", current_task().unwrap().pid.0);
     let token = current_user_token();
@@ -79,7 +97,11 @@ pub fn sys_exec(path: *const u8) -> isize {
 /// If there is not a child process whose pid is same as given, return -1.
 /// Else if there is a child process but it is still running, return -2.
 pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
-    trace!("kernel::pid[{}] sys_waitpid [{}]", current_task().unwrap().pid.0, pid);
+    trace!(
+        "kernel::pid[{}] sys_waitpid [{}]",
+        current_task().unwrap().pid.0,
+        pid
+    );
     let task = current_task().unwrap();
     // find a child process
 
@@ -122,7 +144,18 @@ pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
         "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    let task = current_task().unwrap();
+    // find a child process
+
+    // ---- access current PCB exclusively
+    let inner = task.inner_exclusive_access();
+
+    let us = get_time_us();
+    *translated_refmut(inner.get_user_token(), _ts) = TimeVal {
+        sec: us / 1_000_000,
+        usec: us % 1_000_000,
+    };
+    0
 }
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
@@ -133,7 +166,13 @@ pub fn sys_task_info(_ti: *mut TaskInfo) -> isize {
         "kernel:pid[{}] sys_task_info NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    let task = current_task().unwrap();
+    // find a child process
+
+    // ---- access current PCB exclusively
+
+    *translated_refmut(task.get_user_token(), _ti) = task.get_task_info();
+    0
 }
 
 /// YOUR JOB: Implement mmap.
@@ -142,7 +181,16 @@ pub fn sys_mmap(_start: usize, _len: usize, _port: usize) -> isize {
         "kernel:pid[{}] sys_mmap NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    
+    if current_task()
+        .unwrap()
+        .mmap_program(_start, _len, _port)
+        .is_some()
+    {
+        0
+    } else {
+        -1
+    }
 }
 
 /// YOUR JOB: Implement munmap.
@@ -151,7 +199,11 @@ pub fn sys_munmap(_start: usize, _len: usize) -> isize {
         "kernel:pid[{}] sys_munmap NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    -1
+    if current_task().unwrap().munmap_program(_start, _len).is_some() {
+        0
+    }else {
+        -1
+    }
 }
 
 /// change data segment size
@@ -175,6 +227,7 @@ pub fn sys_spawn(_path: *const u8) -> isize {
 }
 
 // YOUR JOB: Set task priority.
+/// set priority
 pub fn sys_set_priority(_prio: isize) -> isize {
     trace!(
         "kernel:pid[{}] sys_set_priority NOT IMPLEMENTED",
